78f7f008654e6cddf6d538dcfee2a406ab1bcd55
jayant620 <81744390+jayant620@users.noreply.github.com>
2026-07-05 17:54:42 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    Boolean[] dp;
+    public boolean canJump(int[] nums) {
+        dp =new Boolean[nums.length];
+        return dfs(0, nums);
+    }
+
+    private boolean dfs(int i, int[] nums){
+        if(i>=nums.length-1){
+            return true;
+        }
+        if(dp[i] != null) return dp[i];
+        for(int jump=1;jump<=nums[i];jump++){
+            if(dfs(i+jump, nums)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
